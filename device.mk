3c97ba8d3035b6bf511b2fbd5c0e6e2fa4b98401
dianlujitao <dianlujitao@lineageos.org>
2017-11-03 22:51:39 +0800

    op3: Provide vendor camera parameters via shim
    Change-Id: Idb71d93d463a1dc07c72532e95501854d0f8f049

--- device.mk
+++ device.mk
@@ -167,6 +167,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libcamera_parameters_shim \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
