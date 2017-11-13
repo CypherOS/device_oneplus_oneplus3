bcccc0ffe7fc56d4a19e50b298d38d9733de4016
Chris <chris@cypheros.co>
2017-11-13 18:19:48 -0500

    oneplus3: Initial bringup for aoscp
    Change-Id: I66c4752c94445d72e0d144a3de7aae40d9b40d70

--- device.mk
+++ device.mk
@@ -170,7 +170,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     libcamera_shim \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
@@ -211,10 +210,6 @@ PRODUCT_PACKAGES += \
     FOSSConfig.xml \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
-
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -287,10 +282,6 @@ PRODUCT_PACKAGES += \
     lights.msm8996 \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
-
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -433,6 +424,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
