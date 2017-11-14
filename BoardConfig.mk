moetayuko/android_device_oneplus_oneplus3

op3: Import GPS HAL from LA.UM.6.5.r1-04000-8x96.0
Change-Id: Ibcb4cbf494b39087686d0ad351796eca6eabb747

BoardConfig.mk
@@ -168,8 +168,8 @@ endif
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
 # GPS
-TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm8996
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_oneplus3

CypherOS/device_oneplus_oneplus3

BoardConfig.mk
@@ -164,8 +164,8 @@ endif
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
 # GPS
-TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_oneplus3